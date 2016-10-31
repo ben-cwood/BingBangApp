a9db66505e49ff35a51b6284c41f76ef65bed5b4
Benjamin Wood <bw463@cornell.edu>
2016-10-30 23:25:52 -0400

    fixes

--- AlienGameTests/AlienGameTests.swift
+++ AlienGameTests/AlienGameTests.swift
@@ -8,7 +8,7 @@
 
 import XCTest
 
-class AlienGameTests: XCTestCase {
+class BingBangTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
@@ -27,7 +27,7 @@ class AlienGameTests: XCTestCase {
     
     func testPerformanceExample() {
         // This is an example of a performance test case.
-        self.measureBlock {
+        self.measure {
             // Put the code you want to measure the time of here.
         }
     }
